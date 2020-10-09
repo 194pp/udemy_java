051f17f80870bb13b278bbdb5caebcca23d6174c
194pp <zilvinas111@gmail.com>
2020-10-09 04:51:25 +0300

    Input Calculator exercise complete.

---
+++ Java_exercises/src/InputCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        long avg = 0;
+        while(true) {
+            try {
+                int temp = scanner.nextInt();
+                sum+= temp;
+                count++;
+            } catch (Exception e) {
+                break;
+            }
+        }
+        if(count!=0)
+            avg = Math.round((double)sum / count);
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+    }
+}
