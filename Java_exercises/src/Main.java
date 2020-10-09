e5560f37a6677f6aa585e94be1f113387a4e12dc
194pp <zilvinas111@gmail.com>
2020-10-09 05:30:09 +0300

    Paint Job exercise complete.

--- Java_exercises/src/Main.java
+++ Java_exercises/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         //    For testing purposes
-        DiagonalStar.printSquareStar(2);
+        System.out.println(PaintJob.getBucketCount(20,20,25,1));
     }
 }
